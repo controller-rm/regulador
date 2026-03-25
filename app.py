b9863554efc9cb2f57b62e5c513b14b3dd9bfcf5
Controller <controller.rem@gmail.com>
2026-03-25 06:53:50 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from modules import reposicao_estoque, consumo
+
+st.set_page_config(
+    page_title="Painel de Reposição",
+    page_icon="📦",
+    layout="wide"
+)
+
+menu = st.sidebar.selectbox(
+    "Selecione a análise",
+    ["Reposição de Estoque"]
+)
+
+if menu == "Reposição de Estoque":
+    reposicao_estoque.main()
+else:
+    reposicao_estoque.main()
---
+++ database.py
@@ -0,0 +1,21 @@
+import os
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+
+
+def get_connection():
+    return mysql.connector.connect(
+        host=MYSQL_HOST,
+        port=MYSQL_PORT,
+        user=MYSQL_USER,
+        password=MYSQL_PASSWORD,
+        database=MYSQL_DATABASE,
+    )
